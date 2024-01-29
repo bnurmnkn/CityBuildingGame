9f5b2bc17d760bcf15224c6efd66287e0545fce2
Beyza Nur MANKAN <94225615+bnurmnkn@users.noreply.github.com>
2024-01-29 18:28:38 +0300

    building

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public float minXRot;
+    public float maxXRot;
+
+    private float curXRot;
+
+    public float minZoom;
+    public float maxZoom;
+
+    public float zoomSpeed;
+    public float rotationSpeed;
+
+    private float curZoom;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam=Camera.main;
+        curZoom=cam.transform.localPosition.y;
+        curXRot=-50;
+    }
+    private void Update()
+    {
+        curZoom +=Input.GetAxis("Mouse ScrollWheel")*-zoomSpeed;
+        curZoom=Mathf.Clamp(curZoom,minZoom,maxZoom);
+
+        cam.transform.localPosition=UnityEngine.Vector3.up*curZoom;
+        if(Input.GetMouseButton(1))
+        {
+            float x=Input.GetAxis("Mouse X");
+            float y=Input.GetAxis("Mouse Y");
+
+            curXRot+=-y * rotationSpeed;
+            curXRot= Mathf.Clamp(curXRot,minXRot,maxXRot);
+
+            transform.eulerAngles=new UnityEngine.Vector3(curXRot,transform.eulerAngles.y+(x*rotationSpeed),0.0f);
+            
+        }
+        //Movement
+
+        UnityEngine.Vector3 forwart=cam.transform.forward;
+        forwart.y=0.0f;
+        forwart.Normalize();
+
+        UnityEngine.Vector3 right=cam.transform.right;
+
+        float moveX=Input.GetAxisRaw("Horizantal");
+        float moveZ=Input.GetAxisRaw("Vertical");
+
+        UnityEngine.Vector3 dir=forwart* moveZ+right*moveX;
+
+        dir.Normalize();
+        dir *= moveSpeed*Time.deltaTime;
+        transform.position+=dir;
+        
+
+        
+    }
+   
+   
+
+}
