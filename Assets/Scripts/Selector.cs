ee6e69080ff4da24f2b226b6cdd921459f847a7b
Beyza Nur MANKAN <94225615+bnurmnkn@users.noreply.github.com>
2024-02-04 05:49:44 +0300

    add selector script

--- Assets/Scripts/Selector.cs
+++ Assets/Scripts/Selector.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Selector : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera cam;
+    public static Selector instance;
+
+    private void Awake()
+    {
+        instance=this;
+
+    }
     void Start()
     {
-        
+        cam=Camera.main;
     }
-
-    // Update is called once per frame
-    void Update()
+    public Vector3 GetCurTilePosition()
     {
-        
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            return new Vector3(0,-99,0);
+        }
+        Plane plane =new Plane (Vector3.up,Vector3.zero);
+        Ray ray=cam.ScreenPointToRay(Input.mousePosition);
+        float rayOut=0.0f;
+        if(plane.Raycast(ray,out rayOut))
+        {
+            Vector3 newPos=ray.GetPoint(rayOut)-new Vector3(0.05f,0.0f,0.5f);
+            newPos=new Vector3(Mathf.CeilToInt(newPos.x),0.0f,Mathf.CeilToInt(newPos.z));
+            return newPos;
+
+        }
+
+        return new Vector3(0,-99,0);
+
     }
 }
